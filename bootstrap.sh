55471718dadab1bcfd519610b7cdd5c3640d895d
jensdemeyst™ <jens.demeyst.v3917@student.hogent.be>
2016-03-03 09:17:57 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y apache2
+sudo apt-get update
+sudo apt-get install -y apache2
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
